ee10b59794e9ec103dc57ac6d4182f358ab35f9a
Joshua Hecker <jhecker2001@gmail.com>
2023-10-15 10:37:50 -0500

    Updated answer checking.

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -6,7 +6,8 @@ chosen_word = random.choice(word_list)
 
 guess = input("Guess a letter that's in the word: ").lower()
 
-if chosen_word.find(guess) != -1:
-    print(f"Congratulations! The letter {guess} is in the word {chosen_word}!")
-else:
-    print(f"The letter {guess} is not in the word {chosen_word}.")
+for letter in chosen_word:
+    if letter == guess:
+        print("Right")
+    else:
+        print("Wrong")
