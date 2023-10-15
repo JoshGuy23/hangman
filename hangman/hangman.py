a3aada16f379812343dd505bdfdcfd35a152de27
Joshua Hecker <jhecker2001@gmail.com>
2023-10-15 10:34:23 -0500

    Added support for checking the user's answers.

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -5,3 +5,8 @@ word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 
 guess = input("Guess a letter that's in the word: ").lower()
+
+if chosen_word.find(guess) != -1:
+    print(f"Congratulations! The letter {guess} is in the word {chosen_word}!")
+else:
+    print(f"The letter {guess} is not in the word {chosen_word}.")
