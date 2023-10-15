d0396ff0a005f174e8113fcffce8262dc093a915
Joshua Hecker <jhecker2001@gmail.com>
2023-10-15 10:42:51 -0500

    Added support for blanks.

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -4,7 +4,14 @@ word_list = ["aardvark", "baboon", "camel"]
 
 chosen_word = random.choice(word_list)
 
+#Test
+print(f"To test, the word is {chosen_word}.")
+
 guess = input("Guess a letter that's in the word: ").lower()
+
+display = []
+for i in range(1, len(chosen_word)):
+    display.append("_")
 
 for letter in chosen_word:
     if letter == guess:
